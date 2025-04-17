f2a9954cd59c13748a8b81ad6b5ccac33a7bc74d
vibek01 <vibekbin305@gmail.com>
2025-04-17 22:23:07 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { conf } from './conf/conf'
+
+function App() {
+
+  return (
+    <>
+      <h1></h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,7 @@
+export const conf = {
+    appWrireUrl:            String(import.meta.env.VITE_APPWRITE_URL),
+    appWrireProjectId:      String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appWriteDatabaseId:     String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appWrireCollectionId:   String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appWrireBucketId:       String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
