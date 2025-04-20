7fcb483beceb7cbe9d6ba81ab267912c244a1ef5
vibek01 <vibekbin305@gmail.com>
2025-04-20 22:55:06 +0530

    Added auth.js

--- src/main.jsx
+++ src/main.jsx
@@ -44,7 +44,7 @@ const router = createBrowserRouter([
       {
         path: '/all-posts',
         element: (
-          <AuthLayout authentication>
+          <AuthLayout authentication={true}>
             {" "}
             <AllPosts />
           </AuthLayout>
