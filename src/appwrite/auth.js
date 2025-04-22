b5066d8a5dd24730db085133b9eb6b67f0c636f9
vibek01 <vibekbin305@gmail.com>
2025-04-22 20:41:54 +0530

    Updaing major files

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,36 +1,36 @@
-import conf from '../conf/conf';
-import { Client, Account, ID } from 'appwrite';
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
-        console.log("Appwrite Config →", conf.appWriteUrl, conf.appWriteProjectId);
+    constructor() {
         this.client
-            .setEndpoint(conf.appWriteUrl)
-            .setProject(conf.appWriteProjectId)
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
+            
     }
 
-    async createAccount({email, password, name}){
-        try{
-            const userAccount = await this.account.create
-            (ID.unique(), email, password, name);
-            if(userAccount){
-                // Call antoher method
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
                 return this.login({email, password});
             } else {
-                return userAccount;
+               return  userAccount;
             }
         } catch (error) {
             throw error;
         }
     }
 
-    async login({email, password}){
-        try{
-            return await this.account.createEmailPasswordSession(email, password);
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
             throw error;
         }
@@ -40,21 +40,22 @@ export class AuthService {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite Service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
 
         return null;
     }
 
-    async logout(){
-        try{
+    async logout() {
+
+        try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite Service :: logout :: error", error);
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
 const authService = new AuthService();
 
-export default authService;
+export default authService
--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-function Container({children}) {
-  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
-  
+export default function Container({ children, className = '' }) {
+  return (
+    <div className={`w-full max-w-7xl mx-auto px-4 ${className}`}>
+      {children}
+    </div>
+  );
 }
-
-export default Container;
